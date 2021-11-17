0f899825722485c12906c41c30d777c90dab0c27
Maxxim Vigneau <Maxxim.Vigneau@outlook.com>
2021-11-17 18:35:13 -0400

    Added fail safes and Leds

--- final_codes/protective_cat_feeder/protective_cat_feeder.ino
+++ final_codes/protective_cat_feeder/protective_cat_feeder.ino
@@ -18,7 +18,8 @@
 #define SCLK 2 //clock IN is conect to D2 
 #define SERVO1 10 //Servo output pin (must be PWM pin)
 #define SERVO2 11 //Servo output pin (must be PWM pin) 
-
+#define CV_LED 4 //Computer Vison warning light pin (RED LIGHT)
+#define BOWL_LED 5 //Bowl Timeout warning light pin (BLUE LIGHT) 
 
 HX711 scale; //Declaring a object of type HX711
 Servo Joint1; //Declaring a Servo object
@@ -36,11 +37,14 @@ const int bowl_closed = 0; //angle the servos will be at when the bowl is closed
 float weight = 0; //weight read in from scale
 float precent_cat = 0; //value returned from the computer vision program
 int angle = 0; //current location of the servo motors (0 == closed, 90 == open)
+int cv_timeout_count = 0; //Tracks the number of times the computer vision software has failed to return a value in a row
+int bowl_timeout_count = 0; //Tracks the number of times the bowl time out has occured in a row
 
 void computer_vision();
 void openning();
 void is_open();
 void closing();
+void check_failure();
 
 void setup() {
 
@@ -58,6 +62,11 @@ void setup() {
   Joint2.write(angle);
   Joint1.write(angle);
 
+  /*Initalizing Warning Lights*/
+  pinMode(CV_LED, OUTPUT);
+  pinMode(BOWL_LED, OUTPUT);
+  digitalWrite(CV_LED, LOW);
+  digitalWrite(BOWL_LED, LOW);
 }
 
 void loop() {
@@ -72,6 +81,8 @@ void loop() {
     }
 
   }
+
+  check_failure();
 }
 
 void computer_vision() {
@@ -90,6 +101,7 @@ void computer_vision() {
 
     if (Serial.available() > 0) {
       data = true;
+      cv_timeout_count = 0; //reset the counter because the software suceeeded
       precent_cat = Serial.parseFloat();
       Serial.readStringUntil('\n');
     }
@@ -99,6 +111,7 @@ void computer_vision() {
 
   if (data == false) { //if computer_vision_timeout is reached, will assume that it is a cat and judge only based on weight
     precent_cat = 1;
+    cv_timeout_count++; //Computer vision failed, increment the counter
   }
 
 }
@@ -119,15 +132,21 @@ void is_open() {
   unsigned long start_time = millis(); //the time at which the bowl is completely opened
   unsigned long current_time = millis();
   bool cat = true; //is the cat still on the weight scale
+  bool timeout = false; //has bowl_timeout occured
 
-  while (((current_time - start_time) < bowl_timeout) && (cat == true)) {
+  while ((timeout == false) && (cat == true)) {
 
     weight = scale.get_units(15);
     if ((weight > cat_max) || (weight < cat_min)) { //if the weight leaves the bounds of the cat, will assume the cat is no longer there or a dog is there
       cat = false;
+      bowl_timeout_count = 0; //the timeout didn't occur, reset the count
     }
 
     current_time = millis();
+    if ((current_time - start_time) >= bowl_timeout) {
+      timeout = true;
+      bowl_timeout_count++; //timeout occured, increment the count
+    }
   }
 
   closing();
@@ -139,5 +158,27 @@ void closing() {
     angle--;
     Joint1.write(angle);
     Joint2.write(angle);
+  }
+}
+
+void check_failure() {
+  bool reset = false;
+
+  if (cv_timeout_count >= 5) {
+    while (reset == false) {
+      digitalWrite(CV_LED, HIGH);
+      delay(250);
+      digitalWrite(CV_LED, LOW);
+      delay(250);
+    }
+  }
+
+  if (bowl_timeout_count >= 3) {
+    while (reset == false) {
+      digitalWrite(BOWL_LED, HIGH);
+      delay(250);
+      digitalWrite(BOWL_LED, LOW);
+      delay(250);
+    }
   }
 }
